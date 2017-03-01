b810328c92ab547acbfec89905563a745c7c5946
Pavlo Pysarchuk <pavel.pysarchuk@gmail.com>
2017-03-01 22:47:32 +0200

    add basicCRUD to Product Manager Module

---
+++ modules/productManager/models/Products.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\modules\productManager\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $categoryId
+ * @property integer $price
+ *
+ * @property Description[] $descriptions
+ * @property Category $category
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'categoryId'], 'required'],
+            [['categoryId', 'price'], 'integer'],
+            [['name'], 'string', 'max' => 64],
+            [['name'], 'unique'],
+            [['categoryId'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['categoryId' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'categoryId' => 'Category ID',
+            'price' => 'Price',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDescriptions()
+    {
+        return $this->hasMany(Description::className(), ['productId' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'categoryId']);
+    }
+}
---
+++ modules/productManager/models/ProductsSearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\modules\productManager\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\modules\productManager\models\Products;
+
+/**
+ * ProductsSearch represents the model behind the search form about `app\modules\productManager\models\Products`.
+ */
+class ProductsSearch extends Products
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'categoryId', 'price'], 'integer'],
+            [['name'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Products::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'categoryId' => $this->categoryId,
+            'price' => $this->price,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name]);
+
+        return $dataProvider;
+    }
+}
